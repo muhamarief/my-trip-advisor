00be1b5c9d36956b2ef09cfade875085ac8bae0a
ariefrahman <arief.rahman279@yahoo.com>
2017-04-18 12:45:22 +0800

    add ajax for sign up failure

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,3 +1,27 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on('turbolinks:load', function(){
+
+  $('#new_user').on('submit', function(event){
+    debugger
+
+    event.preventDefault()
+    // myEvent = event
+    data = $('#new_user').serializeArray()
+    $.ajax({
+      url: "/users",
+      method: "POST",
+      data: data,
+      success: function(response){
+        pTag = document.createElement('p')
+        pTag.innerText = $(response)[0]
+        pTag.style.color = "red";
+        submitButton = $('input[type="submit"]')[0]
+        errorMessage = $(submitButton).parent()
+        $(errorMessage).prepend(pTag)
+        }
+      })
+    })
+  })
