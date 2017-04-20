6f7ec9de319c90794f1955460253d5b382870cc6
ariefrahman <arief.rahman279@yahoo.com>
2017-04-21 01:31:21 +0800

    add basic search bar for welcome#index listings

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,11 +3,13 @@ class WelcomeController < ApplicationController
 
   def index
     @user = current_user
-    # @listings = Listing.all
     @q = Listing.ransack(params[:q])
-    @listings = @q.result(distinct: true)
+    if params[:q]
+      @listings = @q.result
+    else
+      @listings = Listing.all
+    end
     @customer_review_new = CustomerReview.new
-
   end
 
 end
